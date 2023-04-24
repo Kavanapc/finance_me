1d58ea82fcaac771c47c8106220a364698468b37
root <root@ip-172-31-34-172.ap-south-1.compute.internal>
2023-04-24 11:33:35 +0000

    new commit

--- main.tf
+++ main.tf
@@ -1,65 +1,24 @@
-locals {
- vpc_id = "vpc-0ce09d1b3d9fbea49"
- subnet_id = "subnet-010e03a7844f9d355"
- ssh_user = "ubuntu"
- key_name = "kavanapc2023"
- private_key_path = file("/opt/star-agile-banking-finance/kavanapc2023.ppk")
- }
-
-provider "aws" {
- region = "ap-south-1"
- }
-resource "aws_security_group" "sg" {
-  name   = "sg"
-  vpc_id = local.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_instance" "test-server" {
+  ami           = "ami-007855ac798b5175e" 
+  instance_type = "t2.micro" 
+  key_name = "kavanapc2023"
+  vpc_security_group_ids =["sg-071945e4647bcf16f"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./ kavanapc2023.ppk")
+    host     = self.public_ip
   }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "banking_project" {
-  ami                         = "ami-0f8ca728008ff5af4"
-  subnet_id                   = local.subnet_id
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  security_groups             = [aws_security_group.sg.id]
-  key_name                    = local.key_name
-
   provisioner "remote-exec" {
-    inline = ["echo 'Wait until SSH is ready'"]
-
-    connection {
-      type        = "ssh"
-      user        = local.ssh_user
-      private_key = file(local.private_key_path)
-      host        = aws_instance.banking_project.public_ip
-    }
+    inline = [ "echo 'wait to start instance' "]
   }
-provisioner "local-exec" {
-    command = "ansible-playbook  -i ${aws_instance.banking_project.public_ip}, --private-key ${local.private_key_path} banking.yml"
+  tags = {
+    Name = "test-server"
   }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/<Replace-jenkins-project-name>/test-server/finance-playbook.yml "
+  } 
 }
