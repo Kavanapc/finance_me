b3af23f3631da3c253c0d9c8968af82d7f07f550
Kavanapc <120248270+Kavanapc@users.noreply.github.com>
2023-04-27 06:08:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,23 @@ resource "aws_instance" "test-server" {
   instance_type = "t2.micro"
   key_name = "aws_key"
   vpc_security_group_ids =["sg-071945e4647bcf16f"]
-
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("aws_key.pem")
-    host     = self.public_ip
-  }
- provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
   }
   tags = {
     Name = "test-server"
+    }
+  provisioner "local-exec" {
+            command = "sleep 60 && echo 'Instance ready'"
+        }
+ connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("aws_key.pem")
+    host     = aws_instance.test-server.public_ip
   }
+
   provisioner "local-exec" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
  provisioner "local-exec" {
   command = "ansible-playbook /var/lib/jenkins/workspace/FinanceMe/banking.yml "
               }
-}
-
