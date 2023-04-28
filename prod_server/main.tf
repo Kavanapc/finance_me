a6fb8906f6416b28cb9e57c3ae1d3487816c3344
root <root@ip-172-31-34-172.ap-south-1.compute.internal>
2023-04-28 08:37:47 +0000

    prod

---
+++ prod_server/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-02eb7a4783e7e9317"
+  instance_type = "t2.micro"
+  key_name = "aws_key"
+  vpc_security_group_ids =["sg-071945e4647bcf16f"]
+  
+  tags = {
+    Name = "prod-server"
+    }
+  provisioner "local-exec" {
+            command = "sleep 60 && echo 'Instance ready'"
+        }
+ connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("aws_key.pem")
+    host     = aws_instance.prod-server.public_ip
+  }
+
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-prod.public_ip} > inventory "
+  }
+ provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/FinanceMe/banking.yml"
+              }
+}
